f5bbf8474711b98e411260fcdc368ae316ff41c2
mdinesh8818-boop <mdinesh8818@gmail.com>
2026-04-06 01:53:30 -0400

    Update project files

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "wv5zpk",   // <-- add this line
+  projectId: "ww5zpk",   // <-- add this line
   e2e: {
     setupNodeEvents(on, config) {},
   },
